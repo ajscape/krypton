1876ed0752dba487868cc31a8734eef3a35cf2e3
Nibunan G S <nibunangs@vmware.com>
2017-11-23 17:50:04 +0530

    Third cut: Map Reducer DAG....

--- src/main/java/com/vmware/krypton/service/master/JobToTaskGraphTransformerImpl.java
+++ src/main/java/com/vmware/krypton/service/master/JobToTaskGraphTransformerImpl.java
@@ -3,11 +3,14 @@ package com.vmware.krypton.service.master;
 import com.vmware.krypton.controller.master.JobDescription;
 import com.vmware.krypton.model.TaskDescription;
 import com.vmware.krypton.model.TaskGraph;
+import com.vmware.krypton.service.mappers.basic.DefaultMapper;
 import com.vmware.krypton.service.tasks.Combiner;
 import com.vmware.krypton.service.tasks.QueryTask;
 import com.vmware.krypton.service.tasks.ReducerTask;
 import com.vmware.krypton.service.tasks.SplitterTask;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
@@ -24,22 +27,24 @@ public class JobToTaskGraphTransformerImpl implements JobToTaskGraphTransformer 
         dag.addNode(new TaskDescription("1", QueryTask.class.getName()));
 
         TaskDescription splitterTask = new TaskDescription("2", SplitterTask.class.getName());
-        splitterTask.addInputTaskId("1");
         dag.addNode(splitterTask);
+        dag.addEdge("1", Arrays.asList(splitterTask));
 
         IntStream.rangeClosed(3, getNumberOfNodes() + 2)
-                .forEach(taskIdInt -> dag.addNode(new TaskDescription(Objects.toString(taskIdInt), "Mapper")));
+                .forEach(taskIdInt -> dag.addNode(new TaskDescription(Objects.toString(taskIdInt), DefaultMapper.class.getName())));
+        List<TaskDescription> mapperTasks = dag.getNodesByName(DefaultMapper.class.getName());
+        dag.addEdge("2", mapperTasks);
 
         IntStream.rangeClosed(dag.size() + 1, getNumberOfNodes() + dag.size())
                 .forEach(taskIdInt -> dag.addNode(new TaskDescription(Objects.toString(taskIdInt), ReducerTask.class.getName())));
-        dag.addNode(new TaskDescription(Objects.toString(dag.size() + 1), Combiner.class.getName()));
+        List<TaskDescription> reducerTasks = dag.getNodesByName(ReducerTask.class.getSimpleName());
+        mapperTasks.forEach(mapperTask -> dag.addEdge(mapperTask.getTaskId(), reducerTasks));
 
+        TaskDescription combinerTask = new TaskDescription(Objects.toString(dag.size() + 1), Combiner.class.getName());
+        dag.addNode(combinerTask);
+        reducerTasks.forEach(reducerTask -> dag.addEdge(reducerTask.getTaskId(), Arrays.asList(combinerTask)));
 
         return dag;
     }
 
-    private TaskDescription createQueryTask(String query) {
-        TaskDescription xenonQuery = new TaskDescription("1", "XenonQuery");
-        return xenonQuery;
-    }
 }
