5f783043eccae44bd749f63e5c01b95f1e38ae65
Nibunan G S <nibunangs@vmware.com>
2017-11-24 17:36:30 +0530

    Corrected the Combiner's Input argument...

--- src/main/java/com/vmware/krypton/service/tasks/Combiner.java
+++ src/main/java/com/vmware/krypton/service/tasks/Combiner.java
@@ -2,22 +2,27 @@ package com.vmware.krypton.service.tasks;
 
 import com.vmware.krypton.model.Task;
 import com.vmware.krypton.model.TaskContext;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 /**
  * Created by nibunangs on 23-Nov-2017.
  */
 public class Combiner implements Task<Map<String, List<Object>>, Map<String, List<Object>>> {
 
-    private Collection<Map<String, List<Object>>> inputType;
+    private Collection<Map<String, Object>> inputType;
 
     @Override
     public void execute(TaskContext taskContext) {
+        List<Map<String, Object>> inputs = getInput(taskContext);
 
+        Map<String, Object> output = new HashMap<>();
+        inputs.stream().forEach(stringObjectMap -> output.putAll(stringObjectMap));
+
+        taskContext.emitOutput("0", output);
+    }
+
+    private List<Map<String, Object>> getInput(TaskContext taskContext) {
         Collection<Map<String, List<Object>>> input = null;
         try {
             input = taskContext
@@ -25,23 +30,6 @@ public class Combiner implements Task<Map<String, List<Object>>, Map<String, Lis
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
-        List<String> outputTaksIds = new ArrayList<String>(taskContext.getTaskDescription().getOutputTaskIds());
-
-        List<Map<String, List<Object>>> maps = new ArrayList<>(input);
-
-        Map<String, List<Object>> output = new HashMap<>();
-
-        maps.forEach( item -> {
-            item.forEach((k,v)->{
-                output.computeIfPresent(k, (k1, v1) -> {
-                    v1.addAll(v);
-                    return v1;
-                });
-
-                output.computeIfAbsent(k, k1 -> v);
-            });
-        });
-
-        taskContext.emitOutput(outputTaksIds.get(0), output);
+        return new ArrayList(input);
     }
 }
