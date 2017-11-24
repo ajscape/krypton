d94ace5b430aa4e2249e409cac199798faf4df73
Nibunan G S <nibunangs@vmware.com>
2017-11-24 17:57:46 +0530

    Corrected the Combiner's Logic....

--- src/main/java/com/vmware/krypton/service/tasks/Combiner.java
+++ src/main/java/com/vmware/krypton/service/tasks/Combiner.java
@@ -4,6 +4,7 @@ import com.vmware.krypton.model.Task;
 import com.vmware.krypton.model.TaskContext;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by nibunangs on 23-Nov-2017.
@@ -20,6 +21,24 @@ public class Combiner implements Task<Map<String, List<Object>>, Map<String, Lis
         inputs.stream().forEach(stringObjectMap -> output.putAll(stringObjectMap));
 
         taskContext.emitOutput("0", output);
+    }
+
+    public void executeTemp(TaskContext taskContext) {
+        List<Map<String, Object>> inputs = getInput(taskContext);
+
+        Map<String, Object> output = new HashMap<>();
+        inputs.stream().forEach(stringObjectMap -> output.putAll(stringObjectMap));
+
+        List<String> inputTaskIds = taskContext.getTaskDescription().getInputTaskIds().stream()
+                .filter(s -> !s.equals(taskContext.getTaskDescription().getTaskId())).collect(Collectors.toList());
+
+        long noOfCompletedInputTask = inputTaskIds.stream()
+                .filter(inputTaskId -> taskContext.getInputTaskIdToCompletionMap().get(inputTaskId).equals(Boolean.TRUE)).count();
+        if (noOfCompletedInputTask == inputTaskIds.size()) {
+            taskContext.emitOutput("0", output);
+        } else {
+            taskContext.emitOutput(taskContext.getTaskDescription().getTaskId(), output);
+        }
     }
 
     private List<Map<String, Object>> getInput(TaskContext taskContext) {
